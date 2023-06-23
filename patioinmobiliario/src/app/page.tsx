883777881ef8a1dd50eae8903115ff7749b8c797
Karlo Aldrete <karlo.aldretev1@gmail.com>
2023-06-23 01:34:18 -0700

    update

--- patioinmobiliario/src/app/page.tsx
+++ patioinmobiliario/src/app/page.tsx
@@ -1,95 +1,89 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import '../styles/index.css'
+
+import { BsBookmarkHeart } from 'react-icons/bs'
+import { PiBathtubFill } from 'react-icons/pi'
+import { FaBed } from 'react-icons/fa'
+import { BsFillCarFrontFill } from 'react-icons/bs'
+
+import house from '../images/house.jpg'
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+
+      <div className='properties-available'>
+
+        <div className='property'>
+
+          <div className='property-image'>
+
+            <Image src={house} priority={true} alt='house' />
+
+          </div>
+
+          <div className='property-info'>
+
+            <div className='location'>
+
+              <h3>Otay Insurgentes</h3>
+
+              <p>Tijuana, Baja California Norte</p>
+
+            </div>
+
+            <div className='status'>
+
+              <div className='label'>
+
+                <p>Renta</p>
+
+              </div>
+
+              <div className='details'>
+
+                <div className='item'>
+
+                  <p>3</p>
+
+                  <FaBed className='bed-icon' />
+
+                </div>
+
+                <div className='item'>
+
+                  <p>2</p>
+
+                  <PiBathtubFill className='bath-icon' />
+
+                </div>
+
+                <div className='item'>
+
+                  <p>2</p>
+
+                  <BsFillCarFrontFill className='car-icon' />
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div className='price'>
+
+              <p>$1,200 USD</p>
+
+            </div>
+
+          </div>
+
         </div>
+
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
+
 }
